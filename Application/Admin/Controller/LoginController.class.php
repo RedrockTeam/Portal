050a35a874df94d94e66063ebee4dfb079b4b88e
Excelsior-WYH <1419290661@qq.com>
2016-03-22 21:29:53 +0800

    ok

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -2,34 +2,44 @@
 namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
-	
     public function index(){
-    	$this->assign('check_login',U('Admin:login/check_login'));
-    	$this->assign('index',U('Admin:Finder/joblist'));
+	
+    	$verify =U('Admin:login/verify');
+    	$this->assign('verify',$verify);
+    	
+    	$this->assign('check_login', U('Admin:login/check_login'));
+    	$this->assign('index', U('Admin:Finder/joblist'));
     	$this->display('login');
 	}
 		
-
+	public function verify() {
+		$config = array(
+			'fontSize' => 15, // 验证码字体大小
+			'length' => 4, // 验证码位数
+			//'imageH' => 60,
+			//'imageW' => 200,
+		);
+		 
+		$Verify = new \Think\Verify($config);
+		$Verify->entry();
+	}
+		
+	
 	public function check_login(){
 		$username = I('post.username');
 		$password = I('post.password');
-		if($username && $password && $code){
+		if($username && $password){
 			if(C($username)){
-			
 				if( C($username.".password") == $password){
 					session('login_name',$username);
-					echo 'success';
+					$this->ajaxReturn(array('status' => 200));
 				}else{
-						echo '密码错误';
+					$this->ajaxReturn(array('status' => 0));
 				}
-				
 			}else{
-				echo '账号错误';
+				$this->ajaxReturn(array('status'=>100));
 			}
-		}else{
-			echo "非法操作";
 		}
-		
 	}
 
 	public function logout(){
